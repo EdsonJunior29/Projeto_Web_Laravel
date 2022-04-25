b88f76ddcfe8d7ebfb5ba5f8c75e12bfd6289781
Edson Junior <edsonjos61@gmail.com>
2022-04-25 08:27:02 -0300

    Criando Migration e adicionando tabela no banco de dados MYSQL

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -7,10 +7,9 @@ Séries
 @section('conteudo')
 <a href="/series/criar" type="button" class="btn btn-secondary mb-2">Adicionar</a>
 <ul class="list-group">
-    <?php
-    foreach ($series as $serie) : ?>
-        <li class="list-group-item"><?= $serie; ?></li>
-    <?php endforeach; ?>
+    @foreach($series as $serie)
+    <li class="list-group-item"><?= $serie; ?></li>
+    @endforeach
 
 </ul>
 @endsection
