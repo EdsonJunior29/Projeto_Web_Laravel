0e211bd6a10459f4783c0e3c982c635712afb0cc
Edson Junior <edsonjos61@gmail.com>
2022-04-25 15:49:21 -0300

    Adicionando relacionamento entre as classes.

--- app/Models/Temporada.php
+++ app/Models/Temporada.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Temporada extends Model
 {
-    use HasFactory;
+    public function episodios()
+    {
+        return $this->hasMany(Episodio::class);
+    }
+
+    public function serie()
+    {
+        return $this->belongsTo(Serie::class);
+    }
 }
