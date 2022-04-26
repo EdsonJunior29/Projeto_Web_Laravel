ce3073236a1d8d174fa6a8d4245fbaedb3f7ae44
Edson Junior <edsonjos61@gmail.com>
2022-04-26 12:14:42 -0300

    Refatorando funções para remoção de séries.

--- app/services/RemoverSeries.php
+++ app/services/RemoverSeries.php
@@ -5,21 +5,36 @@ namespace App\services;
 use App\Models\Episodio;
 use App\Models\Temporada;
 use App\Serie;
+use Illuminate\Support\Facades\DB;
 
 class RemoverSeries
 {
     public function removerSerie(int $serieId): string
     {
-        $serie = Serie::find($serieId);
-        $nomeDaSerie = $serie->nome;
-        $serie->temporadas->each(function (Temporada $temporada) {
-            $temporada->episodios->each(function (Episodio $episodio) {
-                $episodio->delete();
-            });
-            $temporada->delete();
+        $nomeDaSerie = '';
+        DB::transaction(function () use ($serieId, &$nomeDaSerie) {
+            $serie = Serie::find($serieId);
+            $nomeDaSerie = $serie->nome;
+
+            $this->removerSerieETemporadas($serie);
         });
-        $serie->delete();
 
         return $nomeDaSerie;
+    }
+
+    public function removerSerieETemporadas($serie): void
+    {
+        $serie->temporadas->each(function (Temporada $temporada) {
+            $this->removerTemporada($temporada);
+        });
+        $serie->delete();
+    }
+
+    public function removerTemporada(Temporada $temporada): void
+    {
+        $temporada->episodios->each(function (Episodio $episodio) {
+            $episodio->delete();
+        });
+        $temporada->delete();
     }
 }
