eaad0745bdbeebf8d99595ef0baab039a96eb6fc
Edson Junior <edsonjos61@gmail.com>
2022-04-27 09:46:27 -0300

    Adicionando autenticação com o Breeze.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__ . '/auth.php';
+
 Route::get('/series', 'App\Http\Controllers\SeriesController@index')
     ->name('Lista_series');
 Route::get('/series/criar', 'App\Http\Controllers\SeriesController@create')
