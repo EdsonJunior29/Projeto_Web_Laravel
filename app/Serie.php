3f6274ddde729ef122dec96648d2d400bd84a851
Edson Junior <edsonjos61@gmail.com>
2022-04-28 15:53:43 -0300

    Refatorando o código para apresentar a imagem na view

--- app/Serie.php
+++ app/Serie.php
@@ -4,11 +4,21 @@ namespace App;
 
 use App\Models\Temporada;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage as FacadesStorage;
+use Storage;
 
 class Serie extends Model
 {
     public $timestamps = false;
     protected $fillable = ['nome', 'capa'];
+
+    public function getCapaUrlAttribute()
+    {
+        if ($this->capa) {
+            return FacadesStorage::url($this->capa);
+        }
+        return FacadesStorage::url('serie/sem-imagem.png' . $this->capa);
+    }
 
     public function temporadas()
     {
--- resources/views/temporadas/index.blade.php
+++ resources/views/temporadas/index.blade.php
@@ -1,18 +1,28 @@
 @extends('layout')
 
 @section('cabecalho')
-Temporadas de {{$serie ->nome}}
+    Temporadas de {{ $serie->nome }}
 @endsection
 
 @section('conteudo')
-<ul class="list-group">
-    @foreach($temporadas as $temporada)
-    <li class="list-group-item d-flex justify-content-between">
-        <a href="/temporadas/{{$temporada->id}}/episodios"> Temporada {{ $temporada->numero }}</a>
-        <span class="badge-secondary">
-            {{$temporada->getEpisodiosAssistidos()->count()}} / {{ $temporada->episodios->count() }}
-        </span>
-    </li>
-    @endforeach
-</ul>
+    @if ($serie->capa)
+        <div class="row mb-4 mt-4">
+            <div class="col-md-12 text-center">
+                <a href="{{ $serie->capa_url }}" target="_blank">
+                    <img src="{{ $serie->capa_url }}" alt="image" class="img-thumbnail" height="400px" width="400px">
+                </a>
+            </div>
+        </div>
+    @endif
+
+    <ul class="list-group">
+        @foreach ($temporadas as $temporada)
+            <li class="list-group-item d-flex justify-content-between">
+                <a href="/temporadas/{{ $temporada->id }}/episodios"> Temporada {{ $temporada->numero }}</a>
+                <span class="badge-secondary">
+                    {{ $temporada->getEpisodiosAssistidos()->count() }} / {{ $temporada->episodios->count() }}
+                </span>
+            </li>
+        @endforeach
+    </ul>
 @endsection
